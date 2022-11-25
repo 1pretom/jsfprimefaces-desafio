a7793bf52b18cc8f87cf2e9522caf1612c92031d
Washington Conceição <washingtonldamacenac@gmail.com>
2022-11-25 17:00:26 -0300

    primeiro commit

---
+++ desafioPrimefaces/src/main/java/com/jsfprimefacesproject/desafioPrimefaces/DesafioPrimefacesApplication.java
@@ -0,0 +1,13 @@
+package com.jsfprimefacesproject.desafioPrimefaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPrimefacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPrimefacesApplication.class, args);
+	}
+
+}
